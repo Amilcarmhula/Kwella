880c3542164fbe234094d401f9291bc7ba55170a
Amilcar Mula <amilcarmhula.am@gmail.com>
2024-05-24 19:54:25 +0200

    estruturação geral do site. criação da barra de navegação e seus itens; adição das secção relacionadas aos itens da barra de navegação e criação do footer e suas views

--- Kwella/Controllers/HomeController.cs
+++ Kwella/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Kwella.Controllers
             return View();
         }
 
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
